18aacf5880489430bc2f05dde8bfd39bb5967abe
Tor4ubas <alex1029@yandex.ru>
2023-07-19 20:53:30 +0300

    add capitalize_words

--- str_func.py
+++ str_func.py
@@ -4,3 +4,11 @@ def capitalize_string(input_string):
 my_string = "Привет, мир!"
 capitalized_string = capitalize_string(my_string)
 print(capitalized_string)
+
+def capitalize_words(string): # делает заглавными первые буквы каждого слова в строке,
+# поступившей на вход функции
+
+    words = string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    capitalized_string = ' '.join(capitalized_words)
+    return capitalized_string
