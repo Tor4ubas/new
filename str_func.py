b9fd7bd3f76d46fab5c4c42182c84108999811fc
Tor4ubas <alex1029@yandex.ru>
2023-07-19 20:56:07 +0300

    add docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def capitalize_string(input_string):
     return input_string.upper()
-
+# функцию принимает на вход строку и возвращает ее со всеми заглавными буквами
 my_string = "Привет, мир!"
 capitalized_string = capitalize_string(my_string)
 print(capitalized_string)
